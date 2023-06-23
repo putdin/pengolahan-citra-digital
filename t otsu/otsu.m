d20c82ae4243a14e4bd9fd47b01accad0239de73
Moh Saifuddin <102182119+sayputtdin@users.noreply.github.com>
2023-06-23 20:46:09 +0700

    Add files via upload

---
+++ t otsu/otsu.m
@@ -0,0 +1,120 @@
+function varargout = otsu(varargin)
+% OTSU MATLAB code for otsu.fig
+%      OTSU, by itself, creates a new OTSU or raises the existing
+%      singleton*.
+%
+%      H = OTSU returns the handle to a new OTSU or the handle to
+%      the existing singleton*.
+%
+%      OTSU('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in OTSU.M with the given input arguments.
+%
+%      OTSU('Property','Value',...) creates a new OTSU or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before otsu_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to otsu_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help otsu
+
+% Last Modified by GUIDE v2.5 10-Jun-2023 16:07:30
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @otsu_OpeningFcn, ...
+                   'gui_OutputFcn',  @otsu_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before otsu is made visible.
+function otsu_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to otsu (see VARARGIN)
+
+% Choose default command line output for otsu
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes otsu wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = otsu_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+global gambar;
+[filename,pathname]=uigetfile({'*.png'});
+if ~isequal(filename, 0)
+    gambar = imread(fullfile(pathname,filename));
+    guidata(hObject,handles);
+    axes(handles.axes1);
+    imshow(gambar);
+else
+    return;
+end
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% --- fungsi grayscale
+global gambar;
+global gray;
+gray = rgb2gray(gambar);
+axes(handles.axes2);
+imshow(gray);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+global gray;
+threshold = graythresh(gray);
+otsu = imbinarize(gray,threshold);
+axes(handles.axes3);
+imshow(otsu);
+
+
+
